c3b0ecc975e85435b9dd989b33a7540a39f9e9f6
Phoenix <mingcma2001@gmail.com>
2022-05-27 09:14:18 -0400

    First commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import string
 import json
 import asyncio
 import DiscordUtils
+import wikipedia
 
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -370,7 +371,7 @@ async def append(ctx, *, arg):
 			questions_list = worksheet.col_values(2)
 
 			if split_question[0] in questions_list:
-				await ctx.send('This question is already added.', delete_after = 15)
+				await ctx.send('This question is already added.', delete_aftxer = 15)
 
 			else:
 				first_row = len(questions_list)
@@ -478,21 +479,54 @@ async def share(ctx, arg):
 
 #————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-# @bot.event
-# async def on_command_error(error, ctx):
-# 	if isinstance(error, commands.CommandNotFound):
-# 		await error.send("No such command")
-
-
-
 @bot.command()
-async def paginate(ctx):
-	embed1 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 1")
-	embed2 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 2")
-	embed3 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 3")
-	paginator = DiscordUtils.Pagination.AutoEmbedPaginator(ctx)
-	embeds = [embed1, embed2, embed3]
-	await paginator.run(embeds)
+async def highlow(ctx):
+	deck = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS']
+	deck_count = 52
+	score = 0
+	user_id = ctx.author.id
+
+	index = random.randrange(0, deck_count)
+	last_card = deck[index]
+	del deck[index]
+	deck_count -= 1
+	embedVar = discord.Embed(description = 'Card remaining: {}\nScore: {}'.format(deck_count, score), color = 0xe3e3e3)
+	image_file = discord.File('poker_cards/{}.png'.format(deck[index]), filename = 'image.png')
+	embedVar.set_image(url = "attachment://image.png")
+	await ctx.send(file = image_file, embed = embedVar)
+
+	while deck:
+		high = False
+		low = False
+		index = random.randrange(0, deck_count)
+		deck_count -= 1
+		
+		await bot.wait_for('message', check = lambda message: message.author == ctx.author, message = 'high')
+
+		if user_input == 'high':
+			high = True
+
+		elif user_input == 'low':
+			low = True
+
+		elif user_input == 'quit':
+			break
+
+		last_card = deck[index]
+		embedVar = discord.Embed(description = 'Card remaining: {}\nScore: {}'.format(deck_count, score), color = 0xe3e3e3)
+		image_file = discord.File('poker_cards/{}.png'.format(deck[index]), filename = 'image.png')
+		embedVar.set_image(url = "attachment://image.png")
+		await ctx.send(file = image_file, embed = embedVar)
+
+
+		# new code
+		new_card = deck[index]
+		#if last_card[0] or new_card[0] not 'J' and last_card[0] or new_card[0] not 'Q' and last_card[0] or new_card[0] not 'K':
+		if new_card[0] == 'J' or new_card[0] == 'Q' or new_card == 'K':
+			if last_card[0] != 'J' and last_card[0] != 'Q' and last_card[0] != 'K':
+				score += 1
+
+
 
 
 #————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -509,7 +543,7 @@ async def on_message(ctx):
 			await ctx.channel.send('{}!'.format(random.choice(cuties)))
 
 		elif ctx.content == 'good bot':
-			await ctx.channel.send(file=discord.File('images/horny.png'))
+			await ctx.channel.send(file=discord.File('images/{}.png'.format('horny')))
 
 		elif ctx.content == '.lauli':
 			await ctx.channel.send(file=discord.File('images/amiyathighs1.png'))
